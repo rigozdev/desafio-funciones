6fa981a619699f8d74991cbb43d9b8a424718087
Rodrigo Gonzalez <rodrigo.gonzaleztr@outlook.com>
2022-11-14 17:51:11 -0300

    desafio concluido

--- 4-pagina-web/assets/js/script.js
+++ 4-pagina-web/assets/js/script.js
@@ -4,26 +4,37 @@ const box3 = document.querySelector('#cajita3');
 const box4 = document.querySelector('#cajita4');
 
 /* Cajita1 */
-box1.style.backgroundColor='red';
+box1.style.backgroundColor = 'red';
 box1.style.width = '200px';
-box1.style.height='200px';
+box1.style.height = '200px';
 
 /* Cajita2 */
-box2.style.backgroundColor='green';
+box2.style.backgroundColor = 'green';
 box2.style.width = '200px';
-box2.style.height='200px';
+box2.style.height = '200px';
 
 /* Cajita3 */
-box3.style.backgroundColor='blue';
+box3.style.backgroundColor = 'blue';
 box3.style.width = '200px';
-box3.style.height='200px';
+box3.style.height = '200px';
 
 /* Cajita4 */
-box4.style.backgroundColor='yellow';
+box4.style.backgroundColor = 'yellow';
 box4.style.width = '200px';
-box4.style.height='200px';
+box4.style.height = '200px';
 
 
-let colPurple = 'purple';
+const letraA = 'pink';
+const letraS = 'orange';
+const letraD = 'tomato';
 
 
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'a') {
+        box1.style.backgroundColor=letraA;
+    } else if (event.key === 's') {
+        box2.style.backgroundColor=letraS;
+    } else if (event.key === 'd'){
+        box3.style.backgroundColor=letraD;
+    }
+})
